a133b5d95bde66e447ddf98217a43f043d7a40c0
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-03-01 19:29:42 +0200

    add header and log out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
+import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/Home";
 import { Registration } from "./pages/Registration/Registration";
 import { Login } from "./pages/Login/Login";
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <Header/>
     <div>
       <Routes>
         <Route path="/" element={<Home/>}/>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import css from './Header.module.css';
+
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+
+export const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm('Are you sure you want to log?')) {
+      dispatch(logout());
+      window.localStorage.removeItem('token');
+    }
+  };
+
+  return (
+    <div className={css.root}>
+      <div className={css.inner}>
+        <Link className={css.logo} to="/">
+          <div>DAILY BLOG</div>
+        </Link>
+        <div className={css.buttons}>
+          {isAuth ? (
+            <>
+              <button onClick={onClickLogout} className="button-contained button-error">
+                Log out
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <button className="button-outlined">Log In</button>
+              </Link>
+              <Link to="/register">
+                <button className="button-contained">Register</button>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
