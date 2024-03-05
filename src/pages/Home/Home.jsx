1105aca82d60fb08cc99f3d6cebf131982e1d491
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-03-05 16:05:44 +0200

    add New and Popular functional

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import css from './Home.module.css';
 
@@ -11,23 +11,62 @@ export const Home = () => {
   const { posts, tags } = useSelector(state => state.posts);
   const userData = useSelector(state => state.auth.data);
 
+  const [activeTab, setActiveTab] = useState(null);
+
   const isPostsLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';
 
   React.useEffect(() => {
     dispatch(fetchPost());
-    dispatch(fetchTags())
-  }, [dispatch])
+    dispatch(fetchTags());
+  }, [dispatch]);
+
+  const filteredPosts = () => {
+    if (activeTab === 'New') {
+      // Filter posts created today
+      return isPostsLoading
+        ? [...Array(5)]
+        : posts.items.filter(
+            (post) =>
+              new Date(post.createdAt).toDateString() === new Date().toDateString()
+          );
+    } else if (activeTab === 'Popular') {
+      // Filter posts with viewsCount > 10
+      return isPostsLoading
+        ? [...Array(5)]
+        : posts.items.filter((post) => post.viewsCount > 10);
+    } else {
+      // Return all posts if activeTab is null
+      return isPostsLoading ? [...Array(5)] : posts.items;
+    }
+  };
+
+  const handleTabClick = (tab) => {
+    // If the same tab is clicked again, reset activeTab to null
+    setActiveTab(activeTab === tab ? null : tab);
+  };
 
   return (
     <>
       <div className={css.tabs}>
-        <div className={css.tab}>New</div>
-        <div className={css.tab}>Popular</div>
+        <button
+          className={css.tab}
+          onClick={() => handleTabClick('New')}
+          style={{ fontWeight: activeTab === 'New' ? 'bold' : 'normal' }}
+        >
+          New
+        </button>
+        <button
+          className={css.tab}
+          onClick={() => handleTabClick('Popular')}
+          style={{ fontWeight: activeTab === 'Popular' ? 'bold' : 'normal' }}
+        >
+          Popular
+        </button>
       </div>
       <div className={css.gridContainer}>
         <div className={css.gridItem}>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+          {filteredPosts().map((obj, index) =>
             isPostsLoading ? (
               <Post key={index} isLoading={true} />
             ) : (
@@ -43,7 +82,7 @@ export const Home = () => {
                 tags={obj.tags}
                 isEditable={userData?._id === obj.user._id}
               />
-            ),
+            )
           )}
         </div>
         <div className={css.tagsItem}>
